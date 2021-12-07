2ff484874c1af2c5b7ac496d5b11daa021affd00
Atef Ben Ali <atef.bettaib@gmail.com>
2021-12-07 09:07:54 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "styles/global.css";
+
+import "./i18n";
+
+import Home from "pages";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
