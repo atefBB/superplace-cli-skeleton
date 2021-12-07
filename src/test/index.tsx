f0864645a7732edc9f657078a0f9b2149048dcf5
Atef Ben Ali <atef.bettaib@gmail.com>
2021-12-07 16:41:39 +0100

    chore: clean-up

--- src/test/index.tsx
+++ src/test/index.tsx
@@ -5,17 +5,9 @@ import {
   RenderResult,
 } from "@testing-library/react";
 
-/**
- * Custom renderer example with @testing-library/react
- * You can customize it to your needs.
- *
- * To learn more about customizing renderer,
- * please visit https://testing-library.com/docs/react-testing-library/setup
- */
-
-export const AllTheProviders = ({ children }) => {
-  return <>{children}</>;
-};
+export function AllTheProviders({ children }) {
+  return <React.Fragment>{children}</React.Fragment>;
+}
 
 const render = (ui: ReactElement, options?: Omit<RenderOptions, "queries">) =>
   baseRender(ui, { wrapper: AllTheProviders, ...options }) as RenderResult;
